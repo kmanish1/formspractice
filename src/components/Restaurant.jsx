7c99d52651eb949b439d9bd2a1fa93442bc4d39b
kmanish1 <kmanishreddy11@gmail.com>
2024-10-18 02:41:17 +0530

    search functionality

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -1,9 +1,24 @@
 import Rescard from "./Rescard";
 import resobj from "../utils/constants";
 import Navbar from "./Navbar";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
+
 function Restaurant() {
-  const [curr, setCurr] = useState(resobj);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setCurr(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+  const [definput, setDefinput] = useState("");
+  const [curr, setCurr] = useState([]);
   const header = function (x) {
     return x.info.avgRating > 4;
   };
@@ -11,8 +26,34 @@ function Restaurant() {
   const header1 = function () {
     setCurr(output);
   };
+  const filterlogic = () => {
+    const output1 = curr.filter((restaurant) =>
+      restaurant.info.name.toLowerCase().includes(definput.toLowerCase())
+    );
+    setCurr(output1);
+  };
+  if (curr.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div>
+      <div className="m-4 p-4">
+        <input
+          type="text"
+          placeholder="Enter the name of restaurant"
+          onChange={(e) => {
+            setDefinput(e.target.value);
+          }}
+          value={definput}
+          className="mx-4 border-2 p-4 w-[30vw]"
+        />
+        <button
+          onClick={filterlogic}
+          className="hover:bg-slate-400 p-4 bg-gray-400"
+        >
+          Search
+        </button>
+      </div>
       <button className="" onClick={header1}>
         Top Rated Restaurants
       </button>
---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,40 @@
+const Shimmer=()=>{
+    return (
+      <div className="grid grid-cols-3">
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+        <div className="border-2 bg-gray-400 border-white cursor-pointer m-4 p-4 rounded-lg flex flex-col items-center justify-evenly text-center"></div>
+      </div>
+    );
+}
+
+export default Shimmer;
